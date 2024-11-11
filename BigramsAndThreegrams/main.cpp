30adf8db14a05e483563c2d84c7b377000008c3b
FrancescoMazzola <francesco.mazzola00@gmail.com>
2024-11-11 19:01:24 +0100

    First Parallelized Version

--- BigramsAndThreegrams/main.cpp
+++ BigramsAndThreegrams/main.cpp
@@ -3,20 +3,51 @@
 #include <unordered_map>
 #include <vector>
 #include <sstream>
+#include <omp.h>
 
-// Function to compute bigrams
+// Function to compute bigrams (parallelized)
 void compute_bigrams(const std::vector<std::string>& words, std::unordered_map<std::string, int>& bigram_count) {
-    for (size_t i = 0; i < words.size() - 1; i++) {
-        std::string bigram = words[i] + " " + words[i + 1];
-        bigram_count[bigram]++;
+#pragma omp parallel
+    {
+        std::unordered_map<std::string, int> local_bigrams;
+
+        // Each thread processes a part of the words
+#pragma omp for
+        for (size_t i = 0; i < words.size() - 1; i++) {
+            std::string bigram = words[i] + " " + words[i + 1];
+            local_bigrams[bigram]++;
+        }
+
+        // Merge local results into the global map
+#pragma omp critical
+        {
+            for (const auto& pair : local_bigrams) {
+                bigram_count[pair.first] += pair.second;
+            }
+        }
     }
 }
 
-// Function to compute trigrams
+// Function to compute trigrams (parallelized)
 void compute_trigrams(const std::vector<std::string>& words, std::unordered_map<std::string, int>& trigram_count) {
-    for (size_t i = 0; i < words.size() - 2; i++) {
-        std::string trigram = words[i] + " " + words[i + 1] + " " + words[i + 2];
-        trigram_count[trigram]++;
+#pragma omp parallel
+    {
+        std::unordered_map<std::string, int> local_trigrams;
+
+        // Each thread processes a part of the words
+#pragma omp for
+        for (size_t i = 0; i < words.size() - 2; i++) {
+            std::string trigram = words[i] + " " + words[i + 1] + " " + words[i + 2];
+            local_trigrams[trigram]++;
+        }
+
+        // Merge local results into the global map
+#pragma omp critical
+        {
+            for (const auto& pair : local_trigrams) {
+                trigram_count[pair.first] += pair.second;
+            }
+        }
     }
 }
 
